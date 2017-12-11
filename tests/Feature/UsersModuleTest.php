703378074f88e8d9927546ebf32522537fd74f3c
Jorge Fernandez <jfernandezcolli@gmail.com>
2017-12-10 21:14:17 -0600

    Se agrega la funcion para verificar que se retornen el mensaje y el valor 200 en la ryta de Uusuario.

---
+++ tests/Feature/UsersModuleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UsersModuleTest extends TestCase
+{
+    /**  * @test */
+     
+    function usuarios()
+    {
+        $this->get('/usuarios')
+             ->assertStatus(200)
+             ->assertSee('usuarios');
+    }
+
+    /**  * @test */
+    function detalles_de_usuarios(){
+    	$this->get('/usuarios/5')
+    		 ->assertStatus(200)
+    		 ->assertSee('Mostrando detalles del usuario: 5');
+    }
+}
