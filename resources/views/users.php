6db9b25167a5d54a5511bf5e71150039535ef3e2
Jorge Fernandez <jfernandezcolli@gmail.com>
2017-12-29 22:48:18 -0600

    Se agrega el Helper e en las secciones donde se imprimen los usuarios apara evitar que se ejecute codigo malintencionado.

--- resources/views/users.php
+++ resources/views/users.php
@@ -11,10 +11,10 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<h1>USUARIOS</h1>
+	<h1><?php echo e($title) ?></h1>
 	<ul>
 		<?php foreach ($users as $user): ?>
-			<li> <?php echo $user ?> </li>
+			<li> <?php echo e($user) ?> </li>
 		<?php endforeach; ?>
 	</ul>
 </body>
